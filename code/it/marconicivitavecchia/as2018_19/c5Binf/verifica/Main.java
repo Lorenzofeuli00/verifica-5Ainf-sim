d17f5e482948ff9f2f3640a4c2db91db0b77ed6f
Rick <riccardoforteleoni@icloud.com>
2018-11-10 11:37:20 +0100

    consegna compito

--- code/it/marconicivitavecchia/as2018_19/c5Binf/verifica/Main.java
+++ code/it/marconicivitavecchia/as2018_19/c5Binf/verifica/Main.java
@@ -1,7 +1,7 @@
 
 // TODO Inserire il package corretto
 // nota: potete usare il suggerimento della lampadina a sinistra (scegliete l'unica opzione sensata). 
-package __TODO__;
+package __TODO___;
 
 import javax.swing.*;
 
@@ -12,7 +12,43 @@ public class Main extends JFrame {
 		super.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
 		
 		// TODO Inserire la size in accordo con il wireframe (valori approssimati)
-		super.setSize(__TODO__,__TODO__);
+		super.setSize(500,300);
+		StringBuilder sb=new StringBuilder();
+		sb.append("<html>");
+		sb.append("<h1>Formula 1 Passion</h1>");
+		sb.append("<div style='display table'>");
+        sb.append("<div style='display:table-row'>");
+		
+		sb.append("<div style='display:table-cell'>");
+		sb.append("Pilota");
+		sb.append("Punteggio");
+		
+		sb.append("</div>");
+        sb.append("<div style='display:table-row'>");
+		
+		sb.append("<div style='display:table-cell'>");
+		sb.append("HAMILTON L.");
+		sb.append("</div>");
+		sb.append("<div style='display:table-cell'>");
+		sb.append("358");
+		sb.append("</div>");
+		
+		sb.append("<div style='display:table-cell'>");
+		sb.append("VETTEL S.");
+		sb.append("</div>");
+		sb.append("<div style='display:table-cell'>");
+		sb.append("294");
+		sb.append("</div>");
+		
+		sb.append("<div style='display:table-cell'>");
+		sb.append("RAIKKONEN K.");
+		sb.append("</div>");
+		sb.append("<div style='display:table-cell'>");
+		sb.append("236");
+		sb.append("</div>");
+		
+		
+		sb.append("</html>");
 		
 		// TODO Usare StringBuilder per costruire la stringa HTML
 		
@@ -21,7 +57,8 @@ public class Main extends JFrame {
 		// TODO Convertire lo StringBuilder in String
 		
 		// TODO Assegnare correttamente la stringa create a JLabel
-		super.add(new JLabel(__TODO__));
+		String str=sb.toString();
+		super.add(new JLabel(str));
 		super.setVisible(true);
 		 
 		System.out.println("Verifica Swing-GitHub");
